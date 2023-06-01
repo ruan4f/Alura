e9b83f7d8c1c8a6568a467a3543ce42b61bf8aa5
Ruan Ferreira <ruan.ferreira@lynxspa.com>
2023-06-01 08:44:57 -0300

    Alura - Calculadora de Impostos

---
+++ Java/DesignPatterns/loja/src/br/com/alura/loja/TestesImpostos.java
@@ -0,0 +1,18 @@
+package br.com.alura.loja;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.TipoImposto;
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class TestesImpostos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+		System.out.println(calculadora.calcular(orcamento, TipoImposto.ISS));
+	}
+
+}
