b5cae034c8991c53a1ff613d3d007618fd5246e0
Ruan Ferreira <ruan.ferreira@lynxspa.com>
2023-06-01 08:54:42 -0300

    Alura - Design Patterns aplicando padrão Strategy

--- Java/DesignPatterns/loja/src/br/com/alura/loja/TestesImpostos.java
+++ Java/DesignPatterns/loja/src/br/com/alura/loja/TestesImpostos.java
@@ -3,7 +3,8 @@ package br.com.alura.loja;
 import java.math.BigDecimal;
 
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
-import br.com.alura.loja.imposto.TipoImposto;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.imposto.ISS;
 import br.com.alura.loja.orcamento.Orcamento;
 
 public class TestesImpostos {
@@ -12,7 +13,8 @@ public class TestesImpostos {
 		// TODO Auto-generated method stub
 		Orcamento orcamento = new Orcamento(new BigDecimal("100"));
 		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
-		System.out.println(calculadora.calcular(orcamento, TipoImposto.ISS));
+		
+		System.out.println(calculadora.calcular(orcamento, new ICMS()));
 	}
 
 }
