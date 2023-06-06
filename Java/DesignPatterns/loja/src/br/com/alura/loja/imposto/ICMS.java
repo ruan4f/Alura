a89953610a793684274dc9a68ab79be125b30cba
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-06-05 23:12:45 -0300

    Alura - Design Patterns

--- Java/DesignPatterns/loja/src/br/com/alura/loja/imposto/ICMS.java
+++ Java/DesignPatterns/loja/src/br/com/alura/loja/imposto/ICMS.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 import br.com.alura.loja.orcamento.Orcamento;
 
-public class ICMS {
+public class ICMS implements Imposto {
 
 	public BigDecimal calcular(Orcamento orcamento) {
 		return orcamento.getValor().multiply(new BigDecimal("0.1"));
--- Java/DesignPatterns/loja/src/br/com/alura/loja/imposto/ISS.java
+++ Java/DesignPatterns/loja/src/br/com/alura/loja/imposto/ISS.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 import br.com.alura.loja.orcamento.Orcamento;
 
-public class ISS {
+public class ISS implements Imposto {
 	
 	public BigDecimal calcular(Orcamento orcamento) {
 		return orcamento.getValor().multiply(new BigDecimal("0.06"));
