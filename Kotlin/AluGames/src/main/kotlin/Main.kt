aa0d90d0e9eead8507fe9f58c954309886806269
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2024-01-01 11:28:56 -0300

    Kotlin study - Search game by code

--- Kotlin/AluGames/src/main/kotlin/Main.kt
+++ Kotlin/AluGames/src/main/kotlin/Main.kt
@@ -5,13 +5,20 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
+import java.util.Scanner
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    val leitura = Scanner(System.`in`)
+    println("Digite um código de jogo para buscar:")
+    val busca = leitura.nextLine()
+
+    val endereco = "https://www.cheapshark.com/api/1.0/games?id=$busca"
+
     val client: HttpClient = HttpClient.newHttpClient()
     val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .uri(URI.create(endereco))
         .build()
 
     val response = client.send(request, BodyHandlers.ofString())
