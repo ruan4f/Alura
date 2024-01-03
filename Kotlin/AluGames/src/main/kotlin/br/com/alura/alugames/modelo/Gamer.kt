9bfe5a4883b35386dd2ce0560efd73c17c27641c
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2024-01-02 21:40:50 -0300

    Update Gamer.kt

--- Kotlin/AluGames/src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
+++ Kotlin/AluGames/src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
@@ -1,5 +1,16 @@
 package br.com.alura.alugames.modelo
 
 data class Gamer(val nome:String, val email:String){
+    var dataNascimento:String? = null
+    var usuario:String? = null
+    val idInterno:String? = null
 
+    constructor(nome: String, email: String, dataNascimento:String, usuario:String):this(nome, email) {
+        this.dataNascimento = dataNascimento
+        this.usuario = usuario
+    }
+
+    override fun toString(): String {
+        return super.toString()
+    }
 }
