dffaeef7cc8e70d0ad86033ac2d30a2755f5830d
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2024-01-03 22:11:41 -0300

    Update Gamer.kt

--- Kotlin/AluGames/src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
+++ Kotlin/AluGames/src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
@@ -1,5 +1,7 @@
 package br.com.alura.alugames.modelo
 
+import kotlin.random.Random
+
 data class Gamer(val nome:String, val email:String){
     var dataNascimento:String? = null
     var usuario:String? = null
@@ -8,10 +10,17 @@ data class Gamer(val nome:String, val email:String){
     constructor(nome: String, email: String, dataNascimento:String, usuario:String):this(nome, email) {
         this.dataNascimento = dataNascimento
         this.usuario = usuario
+        criarIdInterno()
     }
 
     override fun toString(): String {
         return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInterno=$idInterno)"
     }
 
+    fun criarIdInterno() {
+        val numero = Random.nextInt(10000)
+        val tag = String.format("%04d", numero)
+
+        idInterno = "$usuario#$tag"
+    }
 }
