7988b5dd9c16db9c08a1050057ab1d0b79605cd9
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2024-01-04 18:07:35 -0300

    Kotlin study

--- Kotlin/AluGames/src/main/kotlin/br/com/alura/alugames/servicos/ConsumoApi.kt
+++ Kotlin/AluGames/src/main/kotlin/br/com/alura/alugames/servicos/ConsumoApi.kt
@@ -1,25 +1,53 @@
 package br.com.alura.alugames.servicos
 
+import br.com.alura.alugames.modelo.InfoGamerJson
 import br.com.alura.alugames.modelo.InfoJogo
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
+
 class ConsumoApi {
-    fun buscaJogo(id: String):InfoJogo {
+
+    fun buscaJogo(id: String): InfoJogo {
         val endereco = "https://www.cheapshark.com/api/1.0/games?id=$id"
 
         val client: HttpClient = HttpClient.newHttpClient()
         val request = HttpRequest.newBuilder()
             .uri(URI.create(endereco))
             .build()
+        val response = client
+            .send(request, HttpResponse.BodyHandlers.ofString())
 
-        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
         val json = response.body()
+
         val gson = Gson()
         val meuInfoJogo = gson.fromJson(json, InfoJogo::class.java)
+
         return meuInfoJogo
+
     }
+
+    fun buscaGamers(): List<InfoGamerJson> {
+        val endereco = "https://raw.githubusercontent.com/jeniblodev/arquivosJson/main/gamers.json"
+
+        val client: HttpClient = HttpClient.newHttpClient()
+        val request = HttpRequest.newBuilder()
+            .uri(URI.create(endereco))
+            .build()
+        val response = client
+            .send(request, HttpResponse.BodyHandlers.ofString())
+
+        val json = response.body()
+
+        val gson = Gson()
+        val meuGamerTipo = object : TypeToken<List<InfoGamerJson>>() {}.type
+        val listaGamer: List<InfoGamerJson> = gson.fromJson(json, meuGamerTipo)
+
+        return listaGamer
+    }
+
 }
