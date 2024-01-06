127d6e1b179dd86572a1df4892ceda77bf2fdc0b
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2024-01-06 18:35:44 -0300

    Kotlin study

--- Kotlin/AluGames/src/main/kotlin/br/com/alura/alugames/modelo/InfoJogoJson.kt
+++ Kotlin/AluGames/src/main/kotlin/br/com/alura/alugames/modelo/InfoJogoJson.kt
@@ -1,7 +1,9 @@
 package br.com.alura.alugames.modelo
 
+import java.math.BigDecimal
+
 data class InfoJogoJson(
     val titulo: String,
     val capa: String,
-    val preco: Double,
+    val preco: BigDecimal,
     val descricao: String)
