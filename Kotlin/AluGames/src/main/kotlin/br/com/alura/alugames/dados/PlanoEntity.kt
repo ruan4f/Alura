f597c744b5bac0b9d017919cbd7c8b12e60b6a77
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2024-01-07 15:55:52 -0300

    Kotlin Study - JPA Inheritance

--- Kotlin/AluGames/src/main/kotlin/br/com/alura/alugames/dados/PlanoEntity.kt
+++ Kotlin/AluGames/src/main/kotlin/br/com/alura/alugames/dados/PlanoEntity.kt
@@ -1,9 +1,19 @@
 package br.com.alura.alugames.dados
 
+import javax.persistence.*
+
+@Entity
+@Table (name = "planos")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TipoPlano", discriminatorType = DiscriminatorType.STRING)
 sealed class PlanoEntity(val tipo: String)
 
+@Entity
+@DiscriminatorValue("Avulso")
 class PlanoAvulsoEntity(tipo: String): PlanoEntity(tipo)
 
+@Entity
+@DiscriminatorValue("Assinatura")
 class PlanoAssinaturaEntity(
     tipo: String,
     val mensalidade: Double,
