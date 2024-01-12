ba3a29eb4abfad839068f3d63a79008bcb7738a2
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2024-01-11 22:19:00 -0300

    Android Study - Java

--- Android/Java/Agenda/app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ Android/Java/Agenda/app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -8,18 +8,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setTitle("Lista de Alunos");
         List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José","Ana"));
 
         ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
