2ac65eb910750172c70710d598a3588946f6bf89
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2024-02-02 18:53:30 -0300

    Android Study - Java

--- Android/Java/Agenda/app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ Android/Java/Agenda/app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -106,7 +106,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
     }
 
     private void configuraAdapter(ListView listaDeAlunos) {
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        adapter = new ArrayAdapter<>(this, R.layout.item_aluno);
         listaDeAlunos.setAdapter(adapter);
     }
 }
